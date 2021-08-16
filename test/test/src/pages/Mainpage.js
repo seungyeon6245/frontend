ea76f5a27d2980c702bbfcc370172f45163f1ce4
seungyeon <20181644@hanbat.ac.kr>
2021-08-16 14:42:10 +0900

    src update

---
+++ test/test/src/pages/Mainpage.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Mainpage.css';
+
+const Mainpage = () => {
+    return (
+        <div>
+            <header></header>
+            <section className="mp1">
+            </section>
+            <section className="mp2">
+            </section>
+            <section className="mp3">
+            </section>
+            <section className="mp4">
+            </section>
+            <section className="mp5">
+            </section>
+            <footer className="footer"></footer>
+
+        </div>
+    );
+};
+
+export default Mainpage;
